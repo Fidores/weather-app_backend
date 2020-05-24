37b2c27defc5667381bdb274582e193f67e871fe
Fidores <45700014+Fidores@users.noreply.github.com>
2020-05-24 23:03:52 +0200

    Fixed Fahrenheit units

--- models/settings.js
+++ models/settings.js
@@ -1,29 +1,29 @@
-const mongooose = require('mongoose');
-const joi = require('@hapi/joi')
+const mongooose = require("mongoose");
+const joi = require("@hapi/joi");
 
 const SettingsSchema = new mongooose.Schema({
-    _id: false,
-    units: {
-        type: String,
-        default: '',
-        enum: [ 'metric', 'imperal', '' ]
-    },
-    lang: {
-        type: String,
-        default: 'pl',
-        enum: [ 'pl', 'eng' ]
-    }
+  _id: false,
+  units: {
+    type: String,
+    default: "",
+    enum: ["metric", "imperial", ""],
+  },
+  lang: {
+    type: String,
+    default: "pl",
+    enum: ["pl", "eng"],
+  },
 });
 
 const validationSchema = joi.object({
-    units: joi.string().valid('metric', 'imperal').allow(''),
-    lang: joi.string().valid('pl', 'eng')
+  units: joi.string().valid("metric", "imperial").allow(""),
+  lang: joi.string().valid("pl", "eng"),
 });
 
 const defaults = {
-    units: '',
-    lang: 'pl'
-}
+  units: "",
+  lang: "pl",
+};
 
 module.exports.SettingsSchema = SettingsSchema;
 module.exports.validationSchema = validationSchema;
